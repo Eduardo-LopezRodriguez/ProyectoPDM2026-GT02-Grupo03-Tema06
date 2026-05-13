425219b07d266c56475f2e8cd32fcef7da35bd8b
Eduardo-LopezRodriguez <112136071+Eduardo-LopezRodriguez@users.noreply.github.com>
2026-05-12 22:22:09 -0600

    Update OpcionMenuDao.kt

--- GradUES/app/src/main/java/com/example/gradues/data/dao/OpcionMenuDao.kt
+++ GradUES/app/src/main/java/com/example/gradues/data/dao/OpcionMenuDao.kt
@@ -6,7 +6,7 @@ import com.example.gradues.data.entities.OpcionMenu
 class OpcionMenuDao(private val dbHelper: DatabaseHelper) {
 
     fun getOpcionesPorRol(nombreRol: String): List<OpcionMenu> {
-        val lista = mutableListOf<OpcionMenu2>()
+        val lista = mutableListOf<OpcionMenu3>()
         val db = dbHelper.readableDatabase
         val sql = """
             SELECT om.* FROM OpcionesMenu om
